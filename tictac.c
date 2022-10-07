6d58bdd8568823bb98b6f2a0b2cd482dfa93e1cb
Melvin <melvingit@gmail.com>
2022-10-07 02:23:29 -0700

    added comments only

---
+++ tictac.c
@@ -0,0 +1,23 @@
+//Tic Tac Toe game between 2 players or player vs computer
+
+int main(){
+    
+    //prompt user for game they wish to play
+
+
+    //read information from console 
+
+
+    //Begin whichever version of the game is chosen
+
+
+    //Have a loop that continues to display the board until a winner or tie occurs
+
+
+    //Ends game and asks if player would like to play again
+
+
+    //restarts game if player wants to play again
+
+    return 0;
+}
